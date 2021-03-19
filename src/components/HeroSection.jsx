8a2ccd0494d42e29573a5ad32852b48b190aa32b
Neo Lee <w0435748@nscc.ca>
2021-03-19 18:15:26 -0300

    Reduce video size

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../App.css";
 import "./HeroSection.css";
-import heroVideo from "../assets/videos/intro-video.mp4";
+import heroVideo from "../assets/videos/intro-video-min.mp4";
 
 function HeroSection() {
   return (
